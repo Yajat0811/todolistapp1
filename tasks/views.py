78a02d9917bdfa23156db3171b5e31cfa3c949ac
yajat kapoor <yajatkapoor@yajats-MacBook-Pro.local>
2023-08-23 08:46:08 +0530

    First commit to project for deploying

---
+++ tasks/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect,get_object_or_404
+from .models import Task
+from .forms import TaskForm
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+    
+def create_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/create_task.html', {'form': form})
+
+def update_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+
+    context = {'form': form, 'task': task}  # Include 'task' in the context
+    return render(request, 'tasks/update_task.html', context)
+
+def delete_task(request, task_id):
+    task = Task.objects.get(pk=task_id)
+    task.delete()
+    return redirect('task_list')
+
+
+# Create your views here.

867dd1454eaea419930aab754b745f8849b6f50b
yajat kapoor <yajatkapoor@yajats-MacBook-Pro.local>
2023-08-23 09:14:17 +0530

    renamedfile to index.html

--- tasks/views.py
+++ tasks/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render, redirect,get_object_or_404
 from .models import Task
 from .forms import TaskForm
 
-def task_list(request):
+def index(request):
     tasks = Task.objects.all()
-    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+    return render(request, 'tasks/index.html', {'tasks': tasks})
     
 def create_task(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('task_list')
+            return redirect('index')
     else:
         form = TaskForm()
     return render(request, 'tasks/create_task.html', {'form': form})
@@ -23,7 +23,7 @@ def update_task(request, task_id):
         form = TaskForm(request.POST, instance=task)
         if form.is_valid():
             form.save()
-            return redirect('task_list')
+            return redirect('index')
     else:
         form = TaskForm(instance=task)
 
@@ -33,7 +33,7 @@ def update_task(request, task_id):
 def delete_task(request, task_id):
     task = Task.objects.get(pk=task_id)
     task.delete()
-    return redirect('task_list')
+    return redirect('index')
 
 
 # Create your views here.
